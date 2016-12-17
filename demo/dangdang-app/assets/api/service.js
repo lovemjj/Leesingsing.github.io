12da4578da8a5228456b5818a5174819855339c2
Leesingsing <15851990699@163.com>
2016-12-17 11:50:38 +0800

    dangdangapp-api
    修改数据路径

--- demo/dangdang-app/assets/api/service.js
+++ demo/dangdang-app/assets/api/service.js
@@ -3,7 +3,7 @@ app.factory('commonService', ['$http', function($http) {
     // 根据分类信息取数据
     service.getData = function(type, callBack) {
         $http({
-            url: '/demo/dangdang-app-angularJs/data/' + type + '.json',
+            url: '/dangdang-app/data/' + type + '.json',
             method: 'get'
         }).then(function(res) {
             console.log('数据获取到了');
