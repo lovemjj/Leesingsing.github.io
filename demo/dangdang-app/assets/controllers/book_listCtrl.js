671fb775de43f82514e05413508ee72074a8f511
Leesingsing <15851990699@163.com>
2018-03-21 12:55:59 +0800

    delete old
    删除之前所有

--- demo/dangdang-app/assets/controllers/book_listCtrl.js
+++
@@ -1,6 +0,0 @@
-app.controller('booklistCtrl', ['$scope', 'commonService','$routeParams', function($scope, commonService,$routeParams) {
-    commonService.getData($routeParams.id ,function(res) {
-      console.log(res.data);
-      $scope.bookData=res.data;
-    });
-}]);
